c5c974e6a1c58e1e90a372cde7c5c5cf8bdc5696
Thiago H. Gomes <84986716+thetheago@users.noreply.github.com>
2022-09-02 18:10:07 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+from re import sub
+
+titulo = input("Digite o titulo do CRM: ")
+titulo = sub("\s", "-", titulo)
+
+print('------------------------------\n')
+print('(^-^*)/\n')
+print('git checkout -b '+titulo)
+titulo = input("")
